ddf4b5e1e16524091bfc30ce7ca0a04245b1752f
davidelunetta <davidelunetta.27@gmail.com>
2023-11-16 15:04:58 +0100

    card component updated

---
+++ src/components/Card.vue
@@ -0,0 +1,78 @@
+<template>
+    <section class="box"
+             @mouseover="hover = true"
+             @mouseleave="hover = false"
+    >
+        <div class="img-box">
+             <img  
+              class="poster"
+              v-if="poster"
+             :src="`https://image.tmdb.org/t/p/w342${poster}`" 
+             :alt="title"
+        />
+        <img 
+             class="poster"
+             v-else
+             src="https://www.altavod.com/assets/images/poster-placeholder.png" 
+             :alt="title"
+        />
+        </div>
+        <div 
+             class="list-box"
+             :class="{active: hover}"
+        >
+            <ul>
+                  <li><h3>Titolo: {{ title }} </h3></li>
+                  <li>Titolo Originale: {{ originalTitle }}</li>
+                  <li>
+                      <img 
+                          v-if="isFlag"
+                          :src="require(`../assets/${language}.png`)" 
+                          :alt="language"
+                      />
+                      <span v-else>{{ language }}</span>
+                  </li>
+                  <li>
+                      <i 
+                         v-for="index in Math.round(vote/2)" 
+                         :key="index"
+                         class="fa-solid fa-star yellow"
+                      >
+                      </i>
+                  </li>
+                  <li class="over">
+                      <div>Overview: {{ overview }}</div> 
+                  </li>
+            </ul>
+            
+        </div>
+    </section>
+  </template>
+  <script>
+  export default {
+      name: 'Card',
+      props: {
+          poster: String,
+          title: String,
+          originalTitle: String,
+          language: String,
+          vote: Number,
+          overview: String
+      },
+      data() {
+          return {
+              availableFlags: ['it','en'],
+              hover: false,
+          };
+      },
+      computed: {
+          isFlag() {
+              return this.availableFlags.includes(this.language)
+          }
+      }
+  };
+  </script>
+  
+  <style scope lang='scss'>
+       @import '../assets/style/main.scss';
+  </style>
